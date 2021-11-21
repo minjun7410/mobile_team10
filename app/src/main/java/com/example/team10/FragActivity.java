fc8d97f013fe37f5c4546a73f83be879df203821
Kieron <39481106+minjun7410@users.noreply.github.com>
2021-11-22 01:30:30 +0900

    set Fragment (main, friend, user info)

--- app/src/main/java/com/example/team10/FragActivity.java
+++ app/src/main/java/com/example/team10/FragActivity.java
@@ -6,9 +6,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.auth.User;
+
 public class FragActivity extends AppCompatActivity {
     FriendActivity friendActivity;
     MainActivity mainActivity;
+    UserActivity userActivity;
+
+    int present_fragment = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,13 +22,35 @@ public class FragActivity extends AppCompatActivity {
 
         mainActivity = (MainActivity) getSupportFragmentManager().findFragmentById(R.id.fragment);
         friendActivity = new FriendActivity();
+        userActivity = new UserActivity();
+        //하단 네비게이션 메뉴의 아이템을 누를 때마다 해당 동작을 실행한다.
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.item_friend:
+                    onFragmentChanged(1);
+                    break;
+                case R.id.item_main:
+                    onFragmentChanged(0);
+                    break;
+                case R.id.item_info:
+                    onFragmentChanged(2);
+                    break;
+            }
+            return true;
+        });
     }
     public void onFragmentChanged(int index) {
-        if (index == 0) {
+        if (index == present_fragment){}
+        else if (index == 0) {
             getSupportFragmentManager().beginTransaction().replace(R.id.container, mainActivity).commit();
         }
         else if (index == 1) {
             getSupportFragmentManager().beginTransaction().replace(R.id.container, friendActivity).commit(); }
+        else if (index == 2) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, userActivity).commit();
+        }
+        present_fragment = index;
     }
 
 }
--- app/src/main/java/com/example/team10/UserActivity.java
+++ app/src/main/java/com/example/team10/UserActivity.java
@@ -1,22 +1,34 @@
 package com.example.team10;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class UserActivity extends AppCompatActivity {
+public class UserActivity extends Fragment {
     private FirebaseAuth firebaseAuth;
     final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_user, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
 
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = firebaseAuth.getCurrentUser();
@@ -24,9 +36,9 @@ public class UserActivity extends AppCompatActivity {
         String uid = currentUser.getUid();
 
         if (currentUser != null ){
-            Toast.makeText(UserActivity.this, uid, Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), uid, Toast.LENGTH_SHORT).show();
         } else{
-            Toast.makeText(UserActivity.this,"not current User", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(),"not current User", Toast.LENGTH_SHORT).show();
         }
     }
 
