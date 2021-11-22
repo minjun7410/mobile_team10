b2120536f6c2a9903323712485d4e3cd2eb8054b
asung <a.city521@gmail.com>
2021-11-22 23:28:49 +0900

    로그아웃 기능 추가

--- app/src/main/java/com/example/team10/UserActivity.java
+++ app/src/main/java/com/example/team10/UserActivity.java
@@ -1,7 +1,10 @@
 package com.example.team10;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +17,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -30,20 +36,6 @@ public class UserActivity extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        BtnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onClickLogoutBtn(view);
-            }
-        });
-
-        BtnWithdraw.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onClickWithdrawBtn(view);
-            }
-        });
         return inflater.inflate(R.layout.activity_user, container, false);
     }
 
@@ -51,14 +43,29 @@ public class UserActivity extends Fragment {
     public void onStart() {
         super.onStart();
 
+        BtnLogout = (Button) getView().findViewById(R.id.btnLogout) ;
+        BtnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickLogoutBtn(v);
+            }
+        });
+
+        BtnWithdraw = (Button) getView().findViewById(R.id.btnWithdraw);
+        BtnWithdraw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickWithdrawBtn(v);
+            }
+        });
+
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-        String email = currentUser.getEmail();
-        String uid = currentUser.getUid();
 
         TextView TvEmail = (TextView) getView().findViewById(R.id.email_userInfo);
 
         if (currentUser != null ){
+            String email = currentUser.getEmail();
             TvEmail.setText(email);
         } else{
             Toast.makeText(getActivity(),"not current User", Toast.LENGTH_SHORT).show();
@@ -67,11 +74,77 @@ public class UserActivity extends Fragment {
     }
 
     public void onClickLogoutBtn(View view){
-        Toast.makeText(getActivity(), "logout", Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getView().getContext());
+        builder.setTitle("로그아웃").setMessage("로그아웃 하시겠습니까?");
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                try {
+                    firebaseAuth.signOut();
+                } catch (Exception e){
+                    Toast.makeText(getActivity(), "로그아웃에 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                Intent intent = new Intent(getView().getContext(), LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                return;
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     public void onClickWithdrawBtn(View view){
-        Toast.makeText(getActivity(), "탈퇴", Toast.LENGTH_SHORT).show();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        String uid = currentUser.getUid();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getView().getContext());
+        builder.setTitle("회원탈퇴").setMessage("정말 탈퇴 하시겠습니까?");
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (currentUser != null)
+                {
+                    Toast.makeText(getActivity(),currentUser.getEmail(), Toast.LENGTH_SHORT).show();
+                    currentUser.delete().addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    //                currentUser.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        if (task.isSuccessful()) {
+//                            Toast.makeText(getActivity(), "탈퇴되었습니다.", Toast.LENGTH_LONG).show();
+//                        }
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_LONG).show();
+//                    }
+//                });
+                }
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                return;
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
 }
