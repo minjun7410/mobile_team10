e35b824113e7b02f1dfc201b085d474308d7ea34
Kieron <39481106+minjun7410@users.noreply.github.com>
2021-11-24 01:51:35 +0900

    매칭 액티비티에 alertDialog 만들었음 (디자인 수정 필요) 다이어로그 안에 친구 추가와 거절 버튼에 대한 onclick 메서드 정의 필요

--- app/src/main/java/com/example/team10/MatchingActivity.java
+++ app/src/main/java/com/example/team10/MatchingActivity.java
@@ -3,12 +3,17 @@ package com.example.team10;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -22,12 +27,6 @@ public class MatchingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matching);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
         FriendListView = findViewById(R.id.FriendListView);
         friendlist = new ArrayList<Friend>();
         friendlist.add(new Friend("감자"));
@@ -39,10 +38,51 @@ public class MatchingActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String text = friendlist.get(i).getNickName();
-                Intent intent = new Intent(MatchingActivity.this ,ChatRoomActivity.class);
-                intent.putExtra("username",text);
-                startActivity(intent);
+
+                View inflatedView = getLayoutInflater().inflate(R.layout.friend_item, null);
+                ImageView iUserIcon = (ImageView) inflatedView.findViewById(R.id.user_icon);
+                TextView iUserNickname = (TextView) inflatedView.findViewById(R.id.user_nickname);
+                TextView iUserTier = (TextView) inflatedView.findViewById(R.id.user_tier);
+                TextView iUserLevel = (TextView) inflatedView.findViewById(R.id.user_level);
+                TextView iUserManner = (TextView) inflatedView.findViewById(R.id.user_manner);
+                TextView iUserMbti = (TextView) inflatedView.findViewById(R.id.user_mbti);
+                iUserIcon.setImageBitmap(friendlist.get(i).user_icon);
+                iUserNickname.setText(friendlist.get(i).user_nickname);
+                iUserTier.setText("Tier: "+friendlist.get(i).user_tier);
+                iUserLevel.setText("Level: "+friendlist.get(i).user_level);
+                iUserManner.setText("Manner: "+friendlist.get(i).user_manner);
+                iUserMbti.setText("MBTI: "+friendlist.get(i).user_mbti);
+
+                final AlertDialog.Builder alt_blt = new AlertDialog.Builder(MatchingActivity.this, R.style.plus_id_register_dialog_style);
+                alt_blt.setCancelable(false)
+                        .setView(inflatedView)
+                        .setPositiveButton("친구 추가", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .setNegativeButton("거절", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        });
+                AlertDialog dialog = alt_blt.create();
+                dialog.show();
+
+
+                //Intent intent = new Intent(MatchingActivity.this ,ChatRoomActivity.class);
+                //intent.putExtra("username",text);
+                //startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
     }
 }
